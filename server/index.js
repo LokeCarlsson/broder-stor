e2d53fccbe1a2c3a51f6de671fcc63e91c12f243
Christoffer Ekberg <ce222pc@student.lnu.se>
2017-01-15 15:47:58 +0100

    Allow CORS

--- server/index.js
+++ server/index.js
@@ -8,6 +8,13 @@ let app = express()
 // SETUP DB
 let db = new sqlite3.Database(DB_NAME);
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
