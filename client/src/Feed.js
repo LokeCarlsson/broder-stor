03d1be10d2aac677f8c0f8f5d9dcc5fe5b0752f5
Christoffer Ekberg <ce222pc@student.lnu.se>
2017-11-26 16:46:23 +0100

    Update

--- client/src/Feed.js
+++ client/src/Feed.js
@@ -1,32 +1,64 @@
 import React, { Component } from 'react';
 import Request from 'react-http-request';
+
+import ChatMessageGetter from './ChatMessageGetter'
+import ChatMessage from './ChatMessage'
 import './Feed.css';
 
 class Feed extends Component {
-  render() {
-    return (
-      <Request
-          url='http://api.broderstor.nu/messages'
-          method='get'
-          accept='application/json'
-          verbose={true}>
-          {
-          ({error, result, loading}) => {
-            if (loading) {
-              return <div>loading...</div>;
-            } else {
 
-              let chatMessages = result.body.map(function(chatMessage) {
-                return  <div>{ chatMessage.username + ": " + chatMessage.body }</div>
-              });
+    constructor() {
+        super()
 
-              return <div>{ chatMessages }</div>;
-            }
-          }
+        this.state = {
+            messages: []
         }
-      </Request>
-    );
-  }
+
+        this.cmg = new ChatMessageGetter()
+
+        this.cmg.on('newMessages', this.addMessages)
+
+    }
+
+    componentDidMount() {
+        this.cmg.initialFetch().then(messages => {
+            if (messages) {
+                this.addMessages(messages)
+            }
+        }).catch(err => {
+            console.error(err)
+        })
+    }
+
+    addMessages(messages) {
+        console.log('messages:', messages)
+        let stateMessageIds = this.state.messages.map((message) => message.id)
+        let newMessages = messages.filter((m) => !stateMessageIds.includes(m.id))
+
+        let stateMessages = this.state.messages.slice()
+        stateMessages.push(...newMessages)
+
+        console.log('state messages:', stateMessages)
+        this.setState({
+            messages: stateMessages
+        })
+    }
+
+    sortMessages(a, b) {
+        return parseFloat(a.received_on) > parseFloat(b.received_on)
+    }
+
+    render() {
+        if (!this.state.messages.length) {
+            return <div>loading...</div>
+        } else {
+            let chatMessages = this.state.messages.map((message) => {
+                return <ChatMessage message={message}/>
+            })
+
+            return <div className='chat-messages-container'>{ chatMessages }</div>
+        }
+    }
 }
 
 export default Feed;
